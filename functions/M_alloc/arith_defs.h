85b2690e8475f7945e19aaff972c9801cf7da86a
Max Horn <max@quendi.de>
2019-11-06 11:40:39 +0100

    Remove dead code

--- functions/M_alloc/arith_defs.h
+++ functions/M_alloc/arith_defs.h
@@ -13,13 +13,3 @@ int *m_alloc_d2(int );
 int *c_alloc_d2(int , int );
 int *re_alloc_d2(int *, int );
 void fr_ee_d2(int *);
-/*====================================================================*\
-|| Pointer auf die verschiedenen m_alloc-Funktionen, die bei der Suche||
-|| nach Fehlern in der Speicherverwaltung helfen koennen.             ||
-\*====================================================================*/
-
-extern int *(*m_alloc ) (int       );
-extern int *(*c_alloc ) (int  , int);
-extern int *(*re_alloc) (void *, int);
-extern void (*fr_ee   ) (void *    );
-
